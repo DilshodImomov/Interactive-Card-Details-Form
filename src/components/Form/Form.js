bc6f1cff52585e9b357c0ac4903fd2c455634545
DilshodImomov <imomovdilshod2004@gmail.com>
2023-08-15 19:23:39 +0500

    Implementing the form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,22 +5,33 @@ const Form = () => {
         <div className="cover">
             <div className="formBox">
                 <label htmlFor="name">CARDHOLDER NAME</label>
-                <input type="text" />
+                <div className="border">
+                    <input type="text" placeholder="e.g. Jane Applesseed" />
+                </div>
                 <label htmlFor="name">CARD NUMBER</label>
-                <input type="text" />
+                <div className="border">
+                    <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
+                </div>
                 <div className="dateCvv">
                     <div className="date">
                         <label htmlFor="date">EXP. DATE (MM/YY)</label>
                         <div className="inputs">
-                            <input type="text" />
-                            <input type="text" />
+                            <div className="border">
+                                <input type="text" placeholder="MM" />
+                            </div>
+                            <div className="border">
+                                <input type="text" placeholder="YY" />
+                            </div>
                         </div>
                     </div>
                     <div className="cvv">
                         <label htmlFor="cvv">CVV</label>
-                        <input type="text" />
+                        <div className="border">
+                            <input type="text" placeholder="e.g. 123" />
+                        </div>
                     </div>
                 </div>
+                <p className="btn">Confirm</p>
             </div>
         </div>
     )
