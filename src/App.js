ad85f0469dc645278b628569ee940f7a42521996
DilshodImomov <imomovdilshod2004@gmail.com>
2023-08-16 19:16:04 +0500

    Finished the desktop version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import Form from './components/Form/Form';
 import Cards from "./components/Cards/Cards";
+import Completed from './components/Completed/Completed';
 
 function App() {
   const [name, setName] = useState("Jane Applesseed");
@@ -9,7 +10,8 @@ function App() {
   const [cardMonth, setCardMonth] = useState("00");
   const [cardYear, setCardYear] = useState("00");
   const [cardCvv, setCardCvv] = useState("000");
-
+  const [component, setComponent] = useState(0);
+  
   const changeName = (event) => {
     let Name = event.target.value;
     if ( Name.length === 0 ) {
@@ -17,53 +19,58 @@ function App() {
     }
     setName(Name);
   }
-
+  
   const changeNumber = (event) => {
     let number = event.target.value;
     if ( number.length === 0 ) {
       number = "0000 0000 0000 0000";
     }
     else {
-      let newNumber = "";
-      for ( let i = 0; i < number.length; i++ ) {
-        if ( i !== 0 && i % 4 === 0 ) {
-          newNumber += ' ';
+      // if ( !(number[number.length - 1] >= '0' && number[number.length - 1] <= '9') ) {
+        //   return;
+        // }
+        let newNumber = "";
+        for ( let i = 0; i < number.length; i++ ) {
+          if ( i !== 0 && i % 4 === 0 ) {
+            newNumber += ' ';
+          }
+          newNumber += number[i];
         }
-        newNumber += number[i];
+        number = newNumber;
       }
-      number = newNumber;
+      setCardNumber(number);
     }
-    setCardNumber(number);
-  }
-
-  const changeMonth = (event) => {
-    let month = event.target.value;
-    if ( month.length === 0 ) {
-      month = "00";
+    
+    const changeMonth = (event) => {
+      let month = event.target.value;
+      if ( month.length === 0 ) {
+        month = "00";
+      }
+      setCardMonth(month);
     }
-    setCardMonth(month);
-  }
-
-  const changeYear = (event) => {
-    let year = event.target.value;
-    if ( year.length === 0 ) {
-      year = "00";
+    
+    const changeYear = (event) => {
+      let year = event.target.value;
+      if ( year.length === 0 ) {
+        year = "00";
+      }
+      setCardYear(year);
     }
-    setCardYear(year);
-  }
-
-  const changeCvv = (event) => {
-    let cvv = event.target.value;
-    if ( cvv.length === 0 ) {
-      cvv = "000";
+    
+    const changeCvv = (event) => {
+      let cvv = event.target.value;
+      if ( cvv.length === 0 ) {
+        cvv = "000";
+      }
+      setCardCvv(cvv);
     }
-    setCardCvv(cvv);
-  }
 
-  return (
-    <div className="background">
+    const components = [<Form changeName={changeName} changeNumber={changeNumber} setCardNumber={setCardNumber} changeMonth={changeMonth} changeYear={changeYear} changeCvv={changeCvv} setComponent={setComponent} />,<Completed />];
+
+    return (
+      <div className="background">
       <Cards name={name} cardNumber={cardNumber} cardMonth={cardMonth} cardYear={cardYear} cardCvv={cardCvv}  />
-      <Form changeName={changeName} changeNumber={changeNumber} changeMonth={changeMonth} changeYear={changeYear} changeCvv={changeCvv} />
+      { components[component] } 
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,38 +1,111 @@
+import { useRef, useState } from "react";
+
 import "./Form.css";
 
-const Form = ({changeName, changeNumber, changeMonth, changeYear, changeCvv}) => {
+const Form = ({changeName, changeNumber, changeMonth, changeYear, changeCvv, setComponent}) => {
+    const [nameError, setNameError] = useState("");
+    const [cardNumberError, setCardNumberError] = useState("");
+    const [dateError, setDateError] = useState("");
+    const [cvvError, setCvvError] = useState("");
+    const cardName = useRef();
+    const cardNumber = useRef();
+    const cardMonth = useRef();
+    const cardYear = useRef();
+    const cardCvv = useRef();
+    let error = 0;
+    let countErrors = 0;
+
+    const validate = ( element, setError, onlyNumber, exactLength ) => {
+        let value = element.current.value;
+        if ( value.length === 0 ) {
+            element.current.classList.add("wrong");
+            setError("Can't be blank!");
+            countErrors += 1;
+            return;
+        }
+        if ( onlyNumber === false ) {
+            for ( let i = 0; i < value.length; i++ ) {
+                if ( !((value[i] >= 'a' && value[i] <= 'z') || (value[i] >= 'A' && value[i] <= 'Z') || value[i] === ' ') ) {
+                    console.log(value[i])
+                    element.current.classList.add("wrong");
+                    setError("Wrong format, letters only!");
+                    countErrors += 1;
+                    return;
+                }
+            }
+        }
+        else {
+            for ( let i = 0; i < value.length; i++ ) {
+                if ( !(value[i] >= '0' && value[i] <= '9') ) {
+                    element.current.classList.add("wrong");
+                    setError("Wrong format, numbers only!");
+                    countErrors += 1;
+                    return;
+                }
+            }
+        }
+        if ( exactLength !== -1 && value.length !== exactLength ) {
+            element.current.classList.add("wrong");
+            setError("Wrong format, must be " + exactLength + " digits long!");
+            countErrors += 1;
+            return;
+        }
+        error = 1;
+        element.current.classList.remove("wrong");
+        setError("");
+    }
+
+    const submit = () => {
+        countErrors = 0;
+        validate(cardName, setNameError, false, -1);
+        validate(cardNumber, setCardNumberError, true, 16);
+        error = 0;
+        validate(cardMonth, setDateError, true, 2);
+        if ( error === 1 ) {
+            validate(cardYear, setDateError, true, 2);
+        }
+        validate(cardCvv, setCvvError, true, 3);
+        if ( countErrors === 0 ) {
+            setComponent(1);
+        }
+    }
 
     return (
         <div className="cover">
             <div className="formBox">
                 <label htmlFor="name">CARDHOLDER NAME</label>
                 <div className="border">
-                    <input type="text" placeholder="e.g. Jane Applesseed" maxLength={25} onChange={ changeName } />
+                    <input type="text" placeholder="e.g. Jane Applesseed" ref={cardName} maxLength={25} onChange={ changeName } />
                 </div>
+                <p className="error">{nameError}</p>
                 <label htmlFor="name">CARD NUMBER</label>
                 <div className="border">
-                    <input type="text" maxLength={16} placeholder="e.g. 0000 0000 0000 0000" onChange={ changeNumber }/>
+                    <input type="text" maxLength={16} placeholder="e.g. 0000 0000 0000 0000" ref={cardNumber} onChange={ changeNumber } />
                 </div>
+                <p className="error">{cardNumberError}</p>
                 <div className="dateCvv">
                     <div className="date">
                         <label htmlFor="date">EXP. DATE (MM/YY)</label>
                         <div className="inputs">
                             <div className="border">
-                                <input type="text" maxLength={2} placeholder="MM" onChange={ changeMonth } />
+                                <input type="text" maxLength={2} placeholder="MM" ref={cardMonth} onChange={ changeMonth } />
                             </div>
+
                             <div className="border">
-                                <input type="text" maxLength={2} placeholder="YY" onChange={ changeYear } />
+                                <input type="text" maxLength={2} placeholder="YY" ref={cardYear} onChange={ changeYear } />
                             </div>
                         </div>
+                        <p className="error">{dateError}</p>
                     </div>
                     <div className="cvv">
                         <label htmlFor="cvv">CVV</label>
                         <div className="border">
-                            <input type="text" maxLength={3} placeholder="e.g. 123" onChange={ changeCvv } />
+                            <input type="text" maxLength={3} ref={cardCvv} placeholder="e.g. 123" onChange={ changeCvv } />
                         </div>
+                        <p className="error">{cvvError}</p>
                     </div>
                 </div>
-                <p className="btn">Confirm</p>
+                <p className="btn" onClick={submit}>Confirm</p>
             </div>
         </div>
     )
